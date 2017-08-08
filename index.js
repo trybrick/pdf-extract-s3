5ca3f11e33e7e7ed0c6584622616622ef1e1b98b
Tom Noogen <tom@noogen.net>
2017-08-08 11:02:09 -0500

    update convert

--- index.js
+++ index.js
@@ -1,124 +1,129 @@
 'use strict';
-let exec = require('child_process').exec;
-let urlParse = require('url');
-let path = require('path');
+let exec = require( 'child_process' ).exec;
+let urlParse = require( 'url' );
+let path = require( 'path' );
 let cfg = {
-  bucket: process.env['AWS_DEFAULT_BUCKET'] || 'brick-web',
+  bucket: process.env[ 'AWS_DEFAULT_BUCKET' ] || 'brick-web',
   basepath: '/tmp/pdf'
 };
-let request = require('request');
-let mkdirp = require('mkdirp');
-let fs = require('fs');
-let recursiveReadSync = require('recursive-readdir-sync');
-let async = require('async');
+let request = require( 'request' );
+let mkdirp = require( 'mkdirp' );
+let fs = require( 'fs' );
+let recursiveReadSync = require( 'recursive-readdir-sync' );
+let async = require( 'async' );
 
-var doTransform = (event, context, callback) => {
-  console.log('executing: ', event.cmd);
-  const child = exec(event.cmd, (error) => {
+var doTransform = ( event, context, callback ) => {
+  console.log( 'executing: ', event.cmd );
+  const child = exec( event.cmd, ( error ) => {
     // Resolve with result of process
-    callback(error, 'Process complete!');
-  });
+    callback( error, 'Process complete!' );
+  } );
 
   // Log process stdout and stderr
-  child.stdout.on('data', console.log);
-  child.stderr.on('data', console.error);
+  child.stdout.on( 'data', console.log );
+  child.stderr.on( 'data', console.error );
 }
 
-var doUpload = (event, context, callback) => {
-  var files = [], rstFiles = [];
+var doUpload = ( event, context, callback ) => {
+  var files = [],
+    rstFiles = [];
   try {
-    files = recursiveReadSync(`${cfg.basepath}/`);
-  } catch (err) {
+    files = recursiveReadSync( `${cfg.basepath}/` );
+  } catch ( err ) {
     files = [];
-    callback(err);
+    callback( err );
     return;
   }
-  console.log('files: ', files.length);
+  console.log( 'files: ', files.length );
 
-  if (files.length <= 0) {
+  if ( files.length <= 0 ) {
     callback();
     return;
   }
 
-  let s3 = require('./s3.js');
-  var q = async.queue(function(f, cb) {
-    var myKey = f.replace('/tmp/', '');
+  let s3 = require( './s3.js' );
+  var q = async.queue( function ( f, cb ) {
+    var myKey = f.replace( '/tmp/', '' );
 
-    rstFiles.push(myKey);
-    // console.log('uploading: ', f, cfg.bucket);
-    s3.upload(cfg.bucket, myKey, f, cb);
-  }, 10);
+    rstFiles.push( myKey );
+    // console.log( 'uploading: ', f, cfg.bucket );
+    s3.upload( cfg.bucket, myKey, f, cb );
+  }, 10 );
 
-  q.drain = (err, results) => {
-    if (err) {
-      callback(err);
+  q.drain = ( err, results ) => {
+    if ( err ) {
+      callback( err );
       return;
     }
 
     var rst = {
       success: true,
-      path: event.dest.replace('/tmp/', ''),
+      path: event.dest.replace( '/tmp/', '' ),
       files: rstFiles
     };
 
-    callback(null, JSON.stringify(rst, null, 2));
+    callback( null, JSON.stringify( rst, null, 2 ) );
   };
 
-  for (var i = 0; i < files.length; i++) {
-    q.push(files[i]);
+  for ( var i = 0; i < files.length; i++ ) {
+    q.push( files[ i ] );
   }
 }
 
-exports.handler = (event, context, callback) => {
+exports.handler = ( event, context, callback ) => {
   // fix params
   event.params = event.params || {};
   event.params.querystring = event.params.querystring || {};
 
-  event.url = (event.url || event.params.querystring.url) + '';
-  console.log('url: ', event.url);
+  event.url = ( event.url || event.params.querystring.url ) + '';
+  console.log( 'url: ', event.url );
 
-  if (!/^(http|https)\:\/\//gmi.test(event.url)) {
-    context.fail('URL is invalid.');
+  if ( !/^(http|https)\:\/\//gmi.test( event.url ) ) {
+    context.fail( 'URL is invalid.' );
     return;
   }
-  event.url = decodeURIComponent(event.url.replace(/\+/g, ' '));
-  event.dpi = parseInt(event.dpi || event.params.querystring.dpi);
-  if (!event.dpi) {
+  event.url = decodeURIComponent( event.url.replace( /\+/g, ' ' ) );
+  event.dpi = parseInt( event.dpi || event.params.querystring.dpi );
+  event.width = parseInt( event.width || event.params.querystring.width || 1600 );
+  if ( !event.dpi ) {
     event.dpi = 150;
   }
 
-  var opt = urlParse.parse(event.url, true);
-  var pathName = decodeURIComponent(opt.pathname);
-  var destPath = path.dirname(pathName);
-  var fileName = pathName.replace(destPath + '/', '');
+  var opt = urlParse.parse( event.url, true );
+  var pathName = decodeURIComponent( opt.pathname );
+  var destPath = path.dirname( pathName );
+  var fileName = pathName.replace( destPath + '/', '' );
+  var legacyName = path.basename( fileName ).replace( ".PDF", "" ).replace( ".pdf", "" );
 
-  event.dest = (event.dest || event.params.querystring.dest || destPath) + '';
+  event.dest = ( event.dest || event.params.querystring.dest || destPath ) + '';
 
   // prefix with basepath: /tmp/pdf
   destPath = `${cfg.basepath}/${event.dest}`;
 
   // use pdf file name as path and download as index.pdf
   event.dest = `${destPath}/${fileName}/`.toLowerCase()
-    .replace('.pdf', '/').replace(/\/+/gi, '/');
+    .replace( '.pdf', '/' ).replace( /\/+/gi, '/' );
 
   // generate shell exec string
-  event.cmd = `./index.sh "${event.dpi}" "${event.dest}"`;
+  event.cmd = `./index.sh "${event.dpi}" "${event.dest}" ${event.width} "${legacyName}.jpg"`;
 
   // make directory before download
-  mkdirp.sync(event.dest);
+  mkdirp.sync( event.dest );
 
   // do download
-  request({ uri: event.url })
-    .pipe(fs.createWriteStream(event.dest + 'index.pdf'))
-    .on('close', function() {
-      doTransform(event, context, (err) => {
-        if (err) {
-          callback(err);
+  request( {
+      uri: event.url
+    } )
+    .pipe( fs.createWriteStream( event.dest + 'index.pdf' ) )
+    .on( 'close', function () {
+      doTransform( event, context, ( err ) => {
+        if ( err ) {
+          callback( err );
           return;
         }
 
         // do upload if no error
-        doUpload(event, context, callback);
-      });
-    });
+        doUpload( event, context, callback );
+      } );
+    } );
 };
--- index.sh
+++ index.sh
@@ -2,8 +2,13 @@
 cwd=$(pwd)   
 file_dpi=$1
 work_dir=$2
+width=$3
+filename=$4
 
 cd "$work_dir"
-$cwd/bin/pdftoppm -jpeg -r $file_dpi -scale-to-x 1600 index.pdf jpeg-1600-page
+$cwd/bin/pdftoppm -jpeg -r $file_dpi -scale-to-x 1200 index.pdf jpeg-1200-page
+$cwd/bin/pdftoppm -jpeg -r $file_dpi -scale-to-x $width index.pdf "jpeg-$width-page"
 $cwd/bin/pdftohtml -p -xml -hidden index.pdf
 $cwd/bin/pdftoppm -r $file_dpi index.pdf ppm-page
+cp jpeg-1200-page-1.jpg index.jpg
+cp jpeg-1200-page-1.jpg "$filename"
