dfd5112065b8805415ed4d6b63771da80bb8b31d
Tom Noogen <tom@noogen.net>
2017-08-09 08:09:53 -0500

    set y scale

--- index.sh
+++ index.sh
@@ -6,9 +6,9 @@ width=$3
 filename=$4
 
 cd "$work_dir"
-$cwd/bin/pdftoppm -jpeg -r $file_dpi -scale-to-x 1200 index.pdf jpeg-1200-page
-$cwd/bin/pdftoppm -jpeg -r $file_dpi -scale-to-x $width index.pdf "jpeg-$width-page"
+$cwd/bin/pdftoppm -jpeg -r $file_dpi -scale-to-x 1000 -scale-to-y -1 index.pdf jpeg-1000-page
+$cwd/bin/pdftoppm -jpeg -r $file_dpi -scale-to-x $width -scale-to-y -1 index.pdf "jpeg-$width-page"
 $cwd/bin/pdftohtml -p -xml -hidden index.pdf
 $cwd/bin/pdftoppm -r $file_dpi index.pdf ppm-page
-cp -f jpeg-1200-page-1.jpg index.jpg
-cp -f jpeg-1200-page-1.jpg "$filename"
+cp -f jpeg-1000-page-1.jpg index.jpg
+cp -f jpeg-1000-page-1.jpg "$filename"
