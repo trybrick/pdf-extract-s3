ef3b0f7e15306fdec8135d21178a9c3dfdedac24
Tom Noogen <tom@noogen.net>
2018-02-02 10:22:28 -0600

    update

--- index.sh
+++ index.sh
@@ -10,5 +10,5 @@ $cwd/bin/pdftoppm -jpeg -r $file_dpi -scale-to-x 1000 -scale-to-y -1 index.pdf j
 $cwd/bin/pdftoppm -jpeg -r $file_dpi -scale-to-x $width -scale-to-y -1 index.pdf "jpeg-$width-page"
 $cwd/bin/pdftohtml -p -xml -hidden index.pdf
 $cwd/bin/pdftoppm -r $file_dpi index.pdf ppm-page
-cp -f jpeg-1000-page-1.jpg index.jpg
-cp -f jpeg-1000-page-1.jpg "$filename"
+cp -f jpeg-1000-page-1.jpg index.jpg || true
+cp -f jpeg-1000-page-1.jpg "$filename" || true
